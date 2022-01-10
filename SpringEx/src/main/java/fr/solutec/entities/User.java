eee661c1bd9cb2c244ed93051e2e5af7012075d2
jonathanandre <joandre000@gmail.com>
2022-01-10 16:37:30 +0100

    User

--- SpringEx/src/main/java/fr/solutec/entities/User.java
+++ SpringEx/src/main/java/fr/solutec/entities/User.java
@@ -1,5 +1,21 @@
 package fr.solutec.entities;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String login;
+	private String mdp;
 }
