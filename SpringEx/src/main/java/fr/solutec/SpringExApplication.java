fc77165a6c1cf657f943f1cef88c787cb67eb091
Spongebobs3000 <you@example.com>
2022-01-11 09:51:41 +0100

    # WARNING: head commit changed in the meantime
     :)

--- SpringEx/src/main/java/fr/solutec/SpringExApplication.java
+++ SpringEx/src/main/java/fr/solutec/SpringExApplication.java
@@ -1,13 +1,35 @@
 package fr.solutec;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.solutec.entities.User;
+import fr.solutec.repository.UserRepository;
+
 @SpringBootApplication
-public class SpringExApplication {
+public class SpringExApplication implements CommandLineRunner {
+	@Autowired
+	private UserRepository userRepos;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringExApplication.class, args);
+		System.out.println("lancement terminé");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("lancement des instructions perso");
+
+		User u1 = new User(null, "login", "mdp");
+		User u2 = new User(null, "login2", "mdp2");
+		User u3 = new User(null, "login3", "mdp3");
+
+		userRepos.save(u1);
+		userRepos.save(u2);
+		userRepos.save(u3);
 	}
 
 }
