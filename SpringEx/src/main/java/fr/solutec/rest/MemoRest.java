9336dd29dc5a175a506ed44001c642c107e89b84
Spongebobs3000 <you@example.com>
2022-01-11 11:44:08 +0100

    gael est un loser

--- SpringEx/src/main/java/fr/solutec/rest/MemoRest.java
+++ SpringEx/src/main/java/fr/solutec/rest/MemoRest.java
@@ -1,9 +1,11 @@
 package fr.solutec.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,4 +35,21 @@ public class MemoRest {
 	public Memo sendMemo(@RequestBody Memo m) {
 		return memoRepos.save(m);
 	}
+
+	@PostMapping("sendPublicMemo")
+	public Memo sendPublicMemo(@RequestBody Memo m) {
+		return memoRepos.save(m);
+	}
+
+	@DeleteMapping("memo/{id}")
+	public boolean deletePerson(@PathVariable Long id) {
+		Optional<Memo> m = memoRepos.findById(id);
+		if (m.isPresent()) {
+			memoRepos.deleteById(id);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 }
